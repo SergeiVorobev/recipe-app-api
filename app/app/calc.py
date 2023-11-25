e04a466a18787d9d01ad03a37767081d10afa333
Mac <mac@MacBook-Pro-Mac.local>
2023-11-25 09:53:52 +0100

    fix: fix lint issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,11 @@
 """_Calculation function_
 """
 
+
 def add(x, y):
     """Add x and y and return result"""
     return x + y
+
 
 def substract(x, y):
     """Substract x and y and return result"""
