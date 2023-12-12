5c23f405d3e48144cd4e64448806255fc87a9ecc
Mac <mac@MacBook-Pro-Mac.local>
2023-12-12 20:46:12 +0100

    fix: fixing lint errors

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,6 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
-# Create your models here.
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -22,7 +21,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password):
         """Create and return a new superuser"""
         user = self.create_user(email, password)
